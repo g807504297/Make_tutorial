428c10904809c7f7f88e012f9ee98be489ec0c28
root <root@worker02.cluster.local>
2022-01-17 19:24:22 +0000

    Makefile level_1 tutorials, this is a generic version

---
+++ Makefile_tutorial/level1/src/foo/foo.hpp
@@ -0,0 +1,7 @@
+#ifndef FOO_HPP
+#define FOO_HPP
+
+int mul(int a, int b);
+
+
+#endif //FOO_HPP
---
+++ Makefile_tutorial/level1/src/main.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "foo/foo.hpp"
+
+int main()
+{
+
+    printf("5 * 5 = %d\n", mul(5,5));
+    return 0;
+}
